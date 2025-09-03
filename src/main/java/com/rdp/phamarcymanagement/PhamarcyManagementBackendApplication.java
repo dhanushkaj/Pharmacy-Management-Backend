a2af9069bf53fd0a730d98af5e08a89c853b1166
Dhanushka Jayathilaka <Dhanushka.Jayathilaka@kaleris.com>
2025-09-03 12:17:23 +0530

    first commit

---
+++ src/main/java/com/rdp/phamarcymanagement/PhamarcyManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rdp.phamarcymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamarcyManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamarcyManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rdp/phamarcymanagement/PhamarcyManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdp.phamarcymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhamarcyManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
