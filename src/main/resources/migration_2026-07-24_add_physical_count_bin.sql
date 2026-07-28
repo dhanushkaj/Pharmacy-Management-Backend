8b3af3a06ce20d80353a272f1e1472704f07f68b
dhanushkaj <dm.jaya63@gmail.com>
2026-07-28 21:06:18 +0530

    10042B_create_perptual_screen - adding contraints to stock movement

--- src/main/resources/migration_2026-07-24_add_physical_count_bin.sql
+++ src/main/resources/migration_2026-07-24_add_physical_count_bin.sql
@@ -11,3 +11,35 @@ ADD CONSTRAINT stock_movement_from_bin_check CHECK (((from_bin)::text = ANY ((AR
 
 ALTER TABLE pharmacy.stock_movement 
 ADD CONSTRAINT stock_movement_to_bin_check CHECK (((to_bin)::text = ANY ((ARRAY['PHYSICAL_COUNT'::character varying, 'GRN'::character varying, 'INVENTORY'::character varying, 'SOLD'::character varying, 'CUSTOMER_RETURN'::character varying, 'SUPPLIER_RETURN'::character varying, 'EXPIRED'::character varying, 'DAMAGED'::character varying])::text[])));
+
+
+
+ALTER TABLE pharmacy.rdp_alert_config
+  DROP CONSTRAINT rdp_alert_config_alert_type_check,
+  ADD CONSTRAINT rdp_alert_config_alert_type_check CHECK (
+    alert_type IN (
+      'EXPIRY_WARNING',
+      'EXPIRY_CRITICAL',
+      'LOW_STOCK',
+      'OUT_OF_STOCK',
+      'PAYMENT_DUE',
+      'PAYMENT_OVERDUE',
+      'NON_MOVING',
+      'OVER_STOCK'
+    )
+  );
+
+ALTER TABLE pharmacy.rdp_alert_logs
+  DROP CONSTRAINT rdp_alert_logs_alert_type_check,
+  ADD CONSTRAINT rdp_alert_logs_alert_type_check CHECK (
+    alert_type IN (
+      'EXPIRY_WARNING',
+      'EXPIRY_CRITICAL',
+      'LOW_STOCK',
+      'OUT_OF_STOCK',
+      'PAYMENT_DUE',
+      'PAYMENT_OVERDUE',
+      'NON_MOVING',
+      'OVER_STOCK'
+    )
+  );
